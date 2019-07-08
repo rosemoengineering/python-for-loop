890ee37d93839a037ba06c2c042498199ce15c60
Rosemo Mühendislik <35057450+rosemoengineering@users.noreply.github.com>
2019-07-08 19:31:41 +0300

    Add files via upload

---
+++ for_loop_example.py
@@ -0,0 +1,38 @@
+
+sınıf= [ "Ali", "Veli", "Kadir", "Ayşe", "Özge" ]
+sıra=0
+
+for i in sınıf:
+
+    print(i)
+
+for i in sınıf:
+    
+    sıra=sıra+1
+    print(sıra, ". öğrencinin adı", i)
+
+
+
+# range function - 1
+
+liste_1=range(10)
+
+for i in liste_1:
+
+    print("Hello", i)
+    
+# range function - 2
+
+liste_2=range(1,11)
+
+for i in liste_2:
+
+    print("Hello", i)
+    
+# range function - 3
+
+liste_3=range(1,11,2)
+
+for i in liste_3:
+
+    print("Hello", i)
